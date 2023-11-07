8d5b8a13998fef805889933f0170765c6efcf8f2
bbzzyyczczeek <bbzzyyczczeek@interia.pl>
2023-11-07 14:28:59 +0100

    data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,12 +2,12 @@
 insert into address (city, zip_code, street, house_number) values ('Tawaramoto', '639-1156', 'Farwell', '5609');
 insert into address (city, zip_code, street, house_number) values ('Quinarayan', '2725', 'Ilene', '75');
 insert into address (city, zip_code, street, house_number) values ('Shebekino', '309296', 'Elgar', '814');
-insert into address (city, zip_code, street, house_number) values ('Huiyi', null, 'Superior', '2881');
-insert into address (city, zip_code, street, house_number) values ('Beni Khiar', null, 'Bellgrove', '007');
-insert into address (city, zip_code, street, house_number) values ('La Luz', null, 'Carey', '90143');
+insert into address (city, zip_code, street, house_number) values ('Huiyi', '12345', 'Superior', '2881');
+insert into address (city, zip_code, street, house_number) values ('Beni Khiar', '432312', 'Bellgrove', '007');
+insert into address (city, zip_code, street, house_number) values ('La Luz', '5679', 'Carey', '90143');
 insert into address (city, zip_code, street, house_number) values ('Phu Sang', '63120', 'Hovde', '12');
 insert into address (city, zip_code, street, house_number) values ('Salamá', '15001', 'Upham', '5');
-insert into address (city, zip_code, street, house_number) values ('Dalongtan', null, 'Oak', '95269');
+insert into address (city, zip_code, street, house_number) values ('Dalongtan', '1289', 'Oak', '95269');
 insert into address (city, zip_code, street, house_number) values ('Can-Avid', '6806', 'Green Ridge', '3');
 
 
@@ -21,3 +21,31 @@ insert into school (name,address_id) values ( 'Ernie',7);
 insert into school (name,address_id) values ( 'Ramsey',8);
 insert into school (name,address_id) values ( 'Dawna',9);
 insert into school (name,address_id) values ( 'Marina',10);
+
+
+insert into restaurant_address (city, zip_code, street, house_number) values ('Tawaramoto', '639-1156', 'Farwell', '5609');
+insert into restaurant_address (city, zip_code, street, house_number) values ('Quinarayan', '2725', 'Ilene', '75');
+insert into restaurant_address (city, zip_code, street, house_number) values ('Shebekino', '309296', 'Elgar', '814');
+insert into restaurant_address (city, zip_code, street, house_number) values ('Huiyi', '12345', 'Superior', '2881');
+insert into restaurant_address (city, zip_code, street, house_number) values ('Beni Khiar', '432312', 'Bellgrove', '007');
+insert into restaurant_address (city, zip_code, street, house_number) values ('La Luz', '5679', 'Carey', '90143');
+insert into restaurant_address (city, zip_code, street, house_number) values ('Phu Sang', '63120', 'Hovde', '12');
+insert into restaurant_address (city, zip_code, street, house_number) values ('Salamá', '15001', 'Upham', '5');
+insert into restaurant_address (city, zip_code, street, house_number) values ('Dalongtan', '1289', 'Oak', '95269');
+insert into restaurant_address (city, zip_code, street, house_number) values ('Can-Avid', '6806', 'Green Ridge', '3');
+
+
+
+
+
+insert into restaurant (restaurant_Name, account, restaurant_address_id) values ('Lesch, Jenkins and Marvin', 'HU68 4424 3432 1006 2676 4896 8827', 1);
+insert into restaurant (restaurant_Name, account, restaurant_address_id) values ('Hettinger, Williamson and Bernhard', 'AT03 2184 6828 2416 6122', 2);
+insert into restaurant (restaurant_Name, account, restaurant_address_id) values ('Collins, Rau and Hirthe', 'MC27 1874 6948 12OF HCMH C0TO R84', 3);
+insert into restaurant (restaurant_Name, account, restaurant_address_id) values ('Ortiz-Dooley', 'ME49 8450 4447 2692 6835 40', 4);
+insert into restaurant (restaurant_Name, account, restaurant_address_id) values ('Gerhold-Thompson', 'GI29 UYRX YWXV YQO2 O8LN TU8', 5);
+insert into restaurant (restaurant_Name, account, restaurant_address_id) values ('Altenwerth, Rogahn and Kuvalis', 'EE97 9880 9057 9650 1048', 6);
+insert into restaurant (restaurant_Name, account, restaurant_address_id) values ('Swift-Kuhn', 'FR54 7826 8018 65LW 3RF2 Z8SC Q63', 7);
+insert into restaurant (restaurant_Name, account, restaurant_address_id) values ('Moore Inc', 'ES03 2337 0462 0629 8748 7066', 8);
+insert into restaurant (restaurant_Name, account, restaurant_address_id) values ('Hettinger and Sons', 'EE49 4847 7208 9791 9988', 9);
+insert into restaurant (restaurant_Name, account, restaurant_address_id) values ('Kuvalis-Wolf', 'TN68 7904 6212 0685 1456 4086', 10);
+
